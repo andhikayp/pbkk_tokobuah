19a3bc2f00b09fda6f033873f3f2d7f59cbe4961
Andhika <andhikay24@gmail.com>
2020-03-01 17:56:42 +0700

    tutorial 1-7

--- application/config/routes.php
+++ application/config/routes.php
@@ -50,6 +50,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |		my-controller/my-method	-> my_controller/my_method
 */
 $route['default_controller'] = 'welcome';
-$route['admin'] = 'admin/overview';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
+
+$route['about'] = 'welcome/about';
+$route['contact'] = 'welcome/contact';
+
+$route['admin'] = 'admin/overview';
